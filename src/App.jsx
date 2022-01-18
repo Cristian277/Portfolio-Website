0795661f9326c32ee4e6ed377007c4ed4a0ae3c5
Cristian <carredondo@csumb.edu>
2022-01-17 17:21:20 -0800

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import Topbar from "./components/topbar/Topbar";
+import Intro from "./components/intro/Intro";
+import Portfolio from "./components/portfolio/Portfolio";
+import Works from "./components/works/Works";
+import Contact from "./components/contact/Contact";
+import "./app.scss"
+
+function App() {
+  return (
+    <div className="app">
+      <Topbar></Topbar>
+      <div className="sections">
+        <Intro></Intro>
+        <Portfolio></Portfolio>
+        <Works></Works>
+        <Contact></Contact>
+      </div>
+    </div>
+  );
+}
+
+export default App;
