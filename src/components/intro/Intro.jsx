09584b9d2dc39cfb84954f724acacf2194ff3350
Cristian <carredondo@csumb.edu>
2022-02-08 21:40:54 -0800

    Started Portfolio Page

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -3,6 +3,21 @@ import "./intro.scss"
 export default function Intro() {
     return (
         <div className="intro" id = "intro">
+            <div className = "left">
+                <div className="imageContainer">
+                    <img src = "assets/CA.png" alt=""></img>
+                </div>
+            </div>
+            <div className = "right">
+                <div className="wrapper">
+                    <h2>Hi There, I'm</h2>
+                    <h1>Cristian Arredondo</h1>
+                    <h3>Software Engineer</h3>
+                </div>
+                <a href="#portfolio">
+                    <img src="assets/white-down-arrow.png"></img>
+                </a>
+            </div>
         </div>
     )
 }
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,8 +1,54 @@
 import "./portfolio.scss"
 
 export default function Portfolio() {
+    /*
+    <button type="button"
+                onClick={(e) => {
+                    e.preventDefault();
+                    window.location.href='http://google.com';
+                }}
+            > Click here</button>
+            */
     return (
         <div className="portfolio" id = "portfolio">
+            <h1>Portfolio</h1>
+            <ul>
+                <li className="active">Featured</li>
+                <li>Web App</li>
+                <li>Mobile App</li>
+                <li>Game Development</li>
+            </ul>
+            <div className="container">
+                <div className="item">
+                    <img src="assets/LearnItPic.png" alt=""/>
+                    <h3>Lost In Translation</h3>
+                </div>
+
+                <div className="item">
+                    <img src="assets/LearnItPic.png" alt=""/>
+                    <h3>Learn It</h3>
+                </div>
+
+                <div className="item">
+                    <img src="assets/LearnItPic.png" alt=""/>
+                    <h3>Game Store</h3>
+                </div>
+
+                <div className="item">
+                    <img src="assets/LearnItPic.png" alt=""/>
+                    <h3>Recipe Search</h3>
+                </div>
+
+                <div className="item">
+                    <img src="assets/LearnItPic.png" alt=""/>
+                    <h3>Banking App</h3>
+                </div>
+
+                <div className="item">
+                    <img src="assets/LearnItPic.png" alt=""/>
+                    <h3>Banking App</h3>
+                </div>
+            </div>
         </div>
     )
 }
